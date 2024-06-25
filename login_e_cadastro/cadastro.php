b103831d5033d334b8b268d2a12df7ce4e75abf0
Matheus Parreiras <matheusbuenompr@gmail.com>
2024-06-24 21:47:03 -0300

    responsividade tela cadastro add

--- login_e_cadastro/cadastro.php
+++ login_e_cadastro/cadastro.php
@@ -48,13 +48,14 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>GreenScore</title>
-        <link rel="stylesheet" href="../css/cadastro.css">
-        <link rel="shortcut icon" href="../assets/icone.png"> <link rel="icon" type="image/png" href="favicon.png">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GreenScore</title>
+    <link rel="stylesheet" href="../css/cadastro.css">
+    <link rel="shortcut icon" href="../assets/icone.png">
+    <link rel="icon" type="image/png" href="favicon.png">
+</head>
     <body>
         <main>
             <form action="cadastro.php" method="POST" class="form">
